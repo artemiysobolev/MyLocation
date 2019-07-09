352cb8d0fa43c3404c2700dc4439f52b7c471585
artemiy <->
2019-07-09 17:00:02 +0300

    Add icons & change color theme

--- app/src/main/java/com/example/mylocation/MainActivity.java
+++ app/src/main/java/com/example/mylocation/MainActivity.java
@@ -30,14 +30,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -86,17 +78,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_deviceinfo) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_messages) {
 
-        } else if (id == R.id.nav_tools) {
+        } else if (id == R.id.nav_photos) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_contacts) {
 
         }
 
