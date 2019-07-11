51a272c6c8737b32698b8a1e70b86ed344430b7e
DianaAlexPo <48028228+DianaAlexPo@users.noreply.github.com>
2019-07-11 23:51:56 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/mylocation/ListContacts.java
@@ -0,0 +1,121 @@
+package com.example.mylocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ListContacts extends AppCompatActivity {
+
+    ListView listView ;
+    ArrayList<String> StoreContacts ;
+    ArrayAdapter<String> arrayAdapter ;
+    Cursor cursor ;
+    String name, phonenumber ;
+    public  static final int RequestPermissionCode  = 1 ;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_contacts);
+
+        listView = (ListView)findViewById(R.id.listview1);
+
+        button = (Button)findViewById(R.id.button1);
+
+        StoreContacts = new ArrayList<String>();
+
+        EnableRuntimePermission();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                GetContactsIntoArrayList();
+
+                arrayAdapter = new ArrayAdapter<String>(
+                        ListContacts.this,
+                        R.layout.contacts,
+                        R.id.textView, StoreContacts
+                );
+
+                listView.setAdapter(arrayAdapter);
+
+
+            }
+        });
+
+    }
+
+    public void GetContactsIntoArrayList(){
+
+        cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null, null, null);
+
+        while (cursor.moveToNext()) {
+
+            name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+
+            phonenumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+            StoreContacts.add(name + " "  + ":" + " " + phonenumber);
+        }
+
+        cursor.close();
+
+    }
+
+    public void EnableRuntimePermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                ListContacts.this,
+                Manifest.permission.READ_CONTACTS))
+        {
+
+            Toast.makeText(ListContacts.this,"CONTACTS permission allows us to Access CONTACTS app", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(ListContacts.this,new String[]{
+                    Manifest.permission.READ_CONTACTS}, RequestPermissionCode);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int RC, String per[], int[] PResult) {
+
+        switch (RC) {
+
+            case RequestPermissionCode:
+
+                if (PResult.length > 0 && PResult[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(ListContacts.this,"Permission Granted, Now your application can access CONTACTS.", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    Toast.makeText(ListContacts.this,"Permission Canceled, Now your application cannot access CONTACTS.", Toast.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
+
+
+}
+
